ceea639c92e61fab7c9368067de0f483dc906749
Productmedia <jcproduction97@gmail.com>
2021-03-12 17:22:48 +0000

    a rough design of the ui

--- index.js
+++ index.js
@@ -15,5 +15,26 @@ app.get("/", function(request, response) {
 
 })
 
+
+
+let post = {
+  title: "title",
+  colorCard: "rgb(val, val, val)",
+  textBox: "orijgirojgirjgirjgirjgirjgirejge",
+  date: "date of post",
+  time: "time of post" 
+}
+
+const user = {
+  username: "productmedia",
+  email: "fijfijfi@grigok.com",
+  password: "should be hashed",
+  id: "1989384595",
+  posts: [post]
+
+}
+
+console.log(user);
+
 app.use(aboutRouter)
 app.listen(process.env.PORT || 3000, () => console.log("App available on http://localhost:3000"));
